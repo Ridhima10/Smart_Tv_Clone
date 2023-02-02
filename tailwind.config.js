217d3915960020c9e325f325c76347545d83029e
Ridhima Jain <ridhimajain862@gmail.com>
2023-02-02 20:44:08 +0530

    Add tailwind related configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#F6F3F3',
+        dark: '#070509',
+        'sub-primary': '#D49C9C',
+        primary: '#CB7777',
+        secondary: '#ABA6A5',
+        'sub-ternary': '#5C4A47',
+        'sub-secondary': '#9E5D54',
+        ternary: '#AA8D74'
+      }
+    },
+    fontFamily: {
+      'sans': ['"Hanken Grotesk"', 'sans-serif'],
+      'serif': ['"Hanken Grotesk"', 'sans-serif'],
+    }
   },
   plugins: [],
 }
